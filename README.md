b065d1eefc5f7dad5ed12e5f5e087d420957bb9b
jaythaceo <jaythaceo@gmail.com>
2015-09-14 19:58:54 -0400

    [chore] Added third_down conversions

---
+++ README.md
@@ -0,0 +1,1 @@
+<h2>NFL CookBook</h2>
---
+++ third_down.py
@@ -0,0 +1,18 @@
+import nflgame
+
+year, week, season_type = 2014, 1, 'REG'
+
+def third_down(teamname, play_gen):
+  attempts = 0
+  conversions = 0
+  for p in play_gen.filter(team=teamname, third_down_att=1):
+    attempts += 1
+    conversions += p.third_down_conv
+  percentage = float(conversions)/ float(attempts)*100
+  return '%s: %s of %s (%.2f%%)' % (
+      teamname, conversions, attempts, percentage)
+
+games = nflgame.games(year, week, kind=season_type)
+for game in games:
+  print third_down(game.home, game.drives.plays())
+  print third_down(game.away, game.drives.plays())
